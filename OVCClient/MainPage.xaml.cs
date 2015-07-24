6a87097b201d30e26cf48fd0de62471861c67b87
Mike <deroodemike@hotmail.com>
2015-07-24 20:37:49 +0200

    Initial commit. Status feed and add status is working

---
+++ OVCClient/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.WindowsAzure.MobileServices;
+using OVCClient.DataObjects;
+using OVCClient.Pages;
+using System;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace OVCClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MainFrame.Navigate(typeof(SendStatus));
+        }
+
+
+        private void MessagesOverview_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(MessagesOverview));
+        }
+
+        private void SendStatus_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(SendStatus));
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private async void SpeechButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create an instance of SpeechRecognizer.
+            var speechRecognizer = new Windows.Media.SpeechRecognition.SpeechRecognizer();
+
+            // Compile the dictation grammar by default.
+            await speechRecognizer.CompileConstraintsAsync();
+
+            // Start recognition.
+            Windows.Media.SpeechRecognition.SpeechRecognitionResult speechRecognitionResult = await speechRecognizer.RecognizeWithUIAsync();
+
+            // Do something with the recognition result.
+            var messageDialog = new Windows.UI.Popups.MessageDialog(speechRecognitionResult.Text, "Text spoken");
+            await messageDialog.ShowAsync();
+        }
+    }
+
+}
---
+++ OVCClient/Pages/MessagesOverview.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using OVCClient.DataObjects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace OVCClient.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MessagesOverview : Page
+    {
+        private MobileServiceCollection<UnitLog, UnitLog> items;
+        private IMobileServiceTable<UnitLog> unitLogTable = App.MobileService.GetTable<UnitLog>();
+
+
+        public MessagesOverview()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await RefreshTodoItems();
+        }
+
+        private async Task RefreshTodoItems()
+        {
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                // This code refreshes the entries in the list view by querying the TodoItems table.
+                // The query excludes completed TodoItems
+                items = await unitLogTable.OrderByDescending(unitLog => unitLog.ReportedTime)
+                    .ToCollectionAsync();
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                exception = e;
+            }
+
+            if (exception != null)
+            {
+                await new MessageDialog(exception.Message, "Error loading items").ShowAsync();
+            }
+            else
+            {
+                ListItems.ItemsSource = items;
+            }
+        }
+
+        private async void Refresh_Click(object sender, RoutedEventArgs e){
+            await RefreshTodoItems();
+        }
+
+        private async void UpdateLog_Click(object sender, RoutedEventArgs e)
+        {
+            await RefreshTodoItems();
+        }
+    }
+}
